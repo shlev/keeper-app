4e2d311079da3037f757a0b94f6cce85c5596347
shlomi <shl.lev.ks@gmail.com>
2021-07-15 12:40:12 +0300

    add Create area to app ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
 
@@ -10,12 +11,14 @@ function App() {
     return (
         <div>
             <Header />
-            {notes.map(note=>
+            <CreateArea />
+            {/* {notes.map(note=>
              <Note 
              key={note.key}
              title = {note.title}
              content = {note.content}    
-             />)}
+             />)} */}
+             <Note key={1} title="Note Title" content="Note Content" />
             <Footer />
         </div>
     );
